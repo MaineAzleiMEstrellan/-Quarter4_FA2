93f4aaf4b8195a0850cce8473041016dade78053
MaineAzleiMEstrellan <maineazlei.estrellan@smc.pshs.edu.ph>
2025-04-14 21:43:29 +0800

    Update JS.js

--- JS.js
+++ JS.js
@@ -1,25 +1,3 @@
-/*function multtable()
-{
-    num1=parseInt(document.getElementById("inp1").value);
-    num2=parseInt(document.getElementById("inp2").value);
-    var result = " ";
-    if( num1<10&&num1>2&&num2<10&&num2>2){
-        
-
-    for (var m = 1; m <= num1; m++) {
-        
-        for (var n = 1; n <= num2; n++) {
-            result += (m*n) + " ";
-        }
-        result += "<br>";
-    }
-    document.getElementById("output").innerHTML=result;
-
-}   else{
-        alert("Please enter numbers between 2 and 10");
-    }
-}
-*/
 function multtable() {
 
     const num1 = parseInt(document.getElementById("inp1").value);
@@ -27,6 +5,7 @@ function multtable() {
     let result = "";
 
     if (num1 > 2 && num1 < 10 && num2 > 2 && num2 < 10) {
+
         result += "<table>";
 
         for (let m = 1; m <= num1; m++) {
@@ -39,8 +18,11 @@ function multtable() {
 
         result += "</table>";
         document.getElementById("output").innerHTML = result;
+        
     } else {
+
         alert("Please enter numbers between 2 and 10");
+
     }
     
 }
